747b989df9bbf3781a5d940b81d11b72af7b8f57
unrexl <Felix@ANSWEB.de>
2025-09-18 17:24:35 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,10 +28,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/x-icon" href="/puplic/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/puplic/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/puplic/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/puplic/apple-touch-icon.png" />
       </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
