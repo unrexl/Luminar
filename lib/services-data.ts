cb30a6057959ba15417c7773d9379be9f7fd1d9b
unrexl <Felix@ANSWEB.de>
2025-10-12 16:58:59 +0200

    Update services-data.ts

--- lib/services-data.ts
+++ lib/services-data.ts
@@ -1146,7 +1146,7 @@ export const services: Service[] = [
     price: 4.1209,
     oldPrice: 4.9451,
     discount: 17,
-    icon: "data:image/svg+xml,%3Csvg role='img' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'%3E%3Ctitle%3EKick%3C/title%3E%3Cpath fill='%2353FC19' d='M1.333 0h8v5.333H12V2.667h2.667V0h8v8H20v2.667h-2.667v2.666H20V16h2.667v8h-8v-2.667H12v-2.666H9.333V24h-8Z'/%3E%3C/svg%3E",
+    icon: "/icons/kick.png",
   },
   {
     id: "2903",
@@ -1156,7 +1156,7 @@ export const services: Service[] = [
     price: 4.3216,
     oldPrice: 5.1859,
     discount: 17,
-    icon: "data:image/svg+xml,%3Csvg role='img' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'%3E%3Ctitle%3EKick%3C/title%3E%3Cpath fill='%2353FC19' d='M1.333 0h8v5.333H12V2.667h2.667V0h8v8H20v2.667h-2.667v2.666H20V16h2.667v8h-8v-2.667H12v-2.666H9.333V24h-8Z'/%3E%3C/svg%3E",
+    icon: "/icons/kick.png",
   },
 
   // Kick Views
@@ -1168,7 +1168,7 @@ export const services: Service[] = [
     price: 0.7036,
     oldPrice: 0.8443,
     discount: 17,
-    icon: "data:image/svg+xml,%3Csvg role='img' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'%3E%3Ctitle%3EKick%3C/title%3E%3Cpath fill='%2353FC19' d='M1.333 0h8v5.333H12V2.667h2.667V0h8v8H20v2.667h-2.667v2.666H20V16h2.667v8h-8v-2.667H12v-2.666H9.333V24h-8Z'/%3E%3C/svg%3E",
+    icon: "/icons/kick.png",
   },
   {
     id: "2905",
@@ -1178,7 +1178,7 @@ export const services: Service[] = [
     price: 1.407,
     oldPrice: 1.6884,
     discount: 17,
-    icon: "data:image/svg+xml,%3Csvg role='img' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'%3E%3Ctitle%3EKick%3C/title%3E%3Cpath fill='%2353FC19' d='M1.333 0h8v5.333H12V2.667h2.667V0h8v8H20v2.667h-2.667v2.666H20V16h2.667v8h-8v-2.667H12v-2.666H9.333V24h-8Z'/%3E%3C/svg%3E",
+    icon: "/icons/kick.png",
   },
   {
     id: "2906",
@@ -1188,7 +1188,7 @@ export const services: Service[] = [
     price: 2.8143,
     oldPrice: 3.3772,
     discount: 17,
-    icon: "data:image/svg+xml,%3Csvg role='img' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'%3E%3Ctitle%3EKick%3C/title%3E%3Cpath fill='%2353FC19' d='M1.333 0h8v5.333H12V2.667h2.667V0h8v8H20v2.667h-2.667v2.666H20V16h2.667v8h-8v-2.667H12v-2.666H9.333V24h-8Z'/%3E%3C/svg%3E",
+    icon: "/icons/kick.png",
   },
   {
     id: "2907",
@@ -1198,7 +1198,7 @@ export const services: Service[] = [
     price: 4.2212,
     oldPrice: 5.0654,
     discount: 17,
-    icon: "data:image/svg+xml,%3Csvg role='img' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'%3E%3Ctitle%3EKick%3C/title%3E%3Cpath fill='%2353FC19' d='M1.333 0h8v5.333H12V2.667h2.667V0h8v8H20v2.667h-2.667v2.666H20V16h2.667v8h-8v-2.667H12v-2.666H9.333V24h-8Z'/%3E%3C/svg%3E",
+    icon: "/icons/kick.png",
   },
 
   // Spotify Followers
