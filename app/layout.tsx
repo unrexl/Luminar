3cbfdc430c31d3c5e3ce23bb84ec83be5a4073c7
unrexl <Felix@ANSWEB.de>
2025-10-12 18:14:56 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,20 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "LS",
+  title: "Luminar Services",
   description:
     "Luminar is one of the best SMM Service Botting Panel, with services for a lot of Social Media Platforms.",
   keywords: "SMM, social media marketing, Instagram followers, TikTok likes, YouTube views, social media services",
+  icons: {
+    icon: [
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+    ]
+  },
   openGraph: {
     title: "LS",
     description:
@@ -28,10 +38,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" type="image/x-icon" href="/puplic/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/puplic/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/puplic/favicon-16x16.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/puplic/apple-touch-icon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
