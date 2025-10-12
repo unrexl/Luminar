317f0a1c814c6c4eff1038523453e3ac075c3c23
unrexl <Felix@ANSWEB.de>
2025-10-12 18:19:34 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   keywords: "SMM, social media marketing, Instagram followers, TikTok likes, YouTube views, social media services",
   icons: {
     icon: [
-      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon.ico' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
     ],
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
       "Luminar is one of the best SMM Service Botting Panel, with services for a lot of Social Media Platforms.",
     type: "website",
   },
-    generator: 'Luminar SMM Panel v1.0'
+  generator: 'Luminar SMM Panel v1.0'
 }
 
 export default function RootLayout({
@@ -37,13 +37,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
